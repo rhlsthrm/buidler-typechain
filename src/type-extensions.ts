d4b4203a1d421b5ec0b4718807f0ead6a4239b3d
Patricio Palladino <email@patriciopalladino.com>
2020-10-24 11:39:17 -0300

    Remove unnecessary keyword

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -3,11 +3,11 @@ import "hardhat/types/config";
 import { TypechainConfig, TypechainUserConfig } from "./types";
 
 declare module "hardhat/types/config" {
-  export interface HardhatUserConfig {
+  interface HardhatUserConfig {
     typechain?: TypechainUserConfig;
   }
 
-  export interface HardhatConfig {
+  interface HardhatConfig {
     typechain: TypechainConfig;
   }
 }
